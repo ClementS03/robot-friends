3b4e5abcbda74773cf2a72584fe5900452d105a3
ClementS03 <clement.seguin63@gmail.com>
2021-12-01 14:58:40 +0100

    add redux for fetch robots from API

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,25 +1,36 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import SearchBox from '../components/SearchBox';
 import CardList from '../components/CardList';
 // import Scroll from '../components/Scroll';
 import './App.css';
 
-import { setSearchField } from '../actions';
+import { setSearchField, requestRobots } from '../actions';
 
 const App = () => {
 
-    const [robots, setRobots] = useState([]);
-
     const dispatch = useDispatch()
 
-    const { searchField } = useSelector((state) => state);
+    const { searchField } = useSelector((state) => state.searchRobots);
+
+    const { robots, isPending, error } = useSelector(
+        (state) => state.requestRobots
+    )
+
+    const onRequestRobots = () => {
+        dispatch(requestRobots())
+    }
+
+    // useEffect(() => {
+    //     fetch('https://jsonplaceholder.typicode.com/users')
+    //         .then(response => response.json())
+    //         .then(users => setRobots(users));
+    // }, []);
 
     useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(response => response.json())
-            .then(users => setRobots(users));
-    }, []);
+        onRequestRobots();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
 
     const onSearchChange = (event) => {
         dispatch(setSearchField(event.target.value))
@@ -29,9 +40,12 @@ const App = () => {
         return robot.name.toLowerCase().includes(searchField.toLowerCase());
     });
 
-    return !robots.length ? (
-        <h1>Loading...</h1>) :
-        (
+    if (isPending) {
+        return <h1>Loading...</h1>
+    }
+
+    if (!error) {
+        return (
             <div className="tc" >
                 <h1 className='f1'>RoboFriends</h1>
                 <SearchBox searchChange={onSearchChange} />
@@ -39,6 +53,18 @@ const App = () => {
 
             </div>
         )
+
+    }
+    // return !robots.length ? (
+    //     <h1>Loading...</h1>) :
+    //     (
+    //         <div className="tc" >
+    //             <h1 className='f1'>RoboFriends</h1>
+    //             <SearchBox searchChange={onSearchChange} />
+    //             <CardList robots={filterdRobots} />
+
+    //         </div>
+    //     )
 }
 
 export default App;
